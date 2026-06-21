faaa1593196fc321e961299e1d732c8cff4f5928
elmoudir <azizelgadi1@gmail.com>
2026-06-21 03:14:24 +0100

    Auto-resolve asset paths so game runs from any directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include "Engine.h"
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <climits>
 
-int main() {
+int main(int argc, char* argv[]) {
+    char exePath[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", exePath, sizeof(exePath) - 1);
+    if (len != -1) {
+        exePath[len] = '\0';
+        char* lastSlash = strrchr(exePath, '/');
+        if (lastSlash) {
+            *lastSlash = '\0';
+            char* secondLast = strrchr(exePath, '/');
+            if (secondLast) *secondLast = '\0';
+            chdir(exePath);
+        }
+    }
     try {
         Engine engine;
         engine.run();
